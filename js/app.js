8c367fd68fc8ce85273ae327de289db849fbdae4
ericbezanson <eric.bezanson@outlook.com>
2016-12-06 16:13:30 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("suggestionbox", ["ngRoute"]);
+app.config(function($routeprovider){
+    $routeprovider
+        .when('/', {
+            controller: 'MainController',
+            templateUrl: 'main.html'
+        }),
+        .when ('/:id',{
+            controller: 'PostController',
+            templateUrl: 'post.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
