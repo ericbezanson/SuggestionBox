f7a32feb666ed6891af5fad84fe5c84ffef4a75c
ericbezanson <eric.bezanson@outlook.com>
2016-12-07 00:17:44 -0400

    updated service to have comments be an array of objects

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -5,25 +5,37 @@ app.factory('suggestions',[function() {
                 id: 1,
                 title: "Bill Belichick should have to coach in a bikini as punishment for deflategate.",
                 upvotes: 0,
-                comments: ['to me those balls are perfect.']
+                comments: [{
+                    comment: ['to me those balls are perfect.'],
+                    upvote: 0
+                }]
             },
             {
                 id: 2,
                 title: "Dairy Queen should bring back the Nerds Blizzard.",
                 upvotes: 0,
-                comments: ['SO MUCH YES']
+                comments: [{
+                    comment: ['SO MUCH YES'],
+                    upvote: 0
+                }]
             },
             {
                 id: 3,
                 title: "Mcdonalds should have the option to buy McNuggets one at a time, like Timbits.",
                 upvotes: 0,
-                comments: ['I think they should serve the sweet and sour sauce inside a small cup, there is never enough!']
+                comments: [{
+                    comment: ['I think they should serve the sweet and sour sauce inside a small cup, there is never enough!'],
+                    upvote: 0
+                }]
             },
             {
                 id: 4,
                 title: "All senior citizens should have life alert.",
                 upvotes: 0,
-                comments: ['they should also strongly concider sham wow, oxyclean and a hoveround']
+                comments: [{
+                    comment: ['they should also strongly concider sham wow, oxyclean and a hoveround'],
+                    upvote: 0
+                }]
             }
         ]
     }
